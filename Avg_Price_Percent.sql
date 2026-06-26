8b476a28da36ced5661f9f75895776c4a09d9cf8
Dhavalkumar510 <127059752+Dhavalkumar510@users.noreply.github.com>
2026-06-26 13:46:23 +0100

    Update Avg_Price_Percent.sql

--- Avg_Price_Percent.sql
+++ Avg_Price_Percent.sql
@@ -2,6 +2,6 @@ SELECT
     Region,
     AVG(Price_Variance_Percent) AS Avg_Price_Variance_Percent
 	
-FROM Housing_Transactions_Master
+FROM Housing_Transactions
 GROUP BY Region
 ORDER BY Avg_Price_Variance_Percent DESC;
