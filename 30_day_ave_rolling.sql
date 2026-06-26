ace073e63496887f7ff0e2c9dc16897363af0eae
Dhavalkumar510 <127059752+Dhavalkumar510@users.noreply.github.com>
2026-06-26 13:46:02 +0100

    Update 30_day_ave_rolling.sql

--- 30_day_ave_rolling.sql
+++ 30_day_ave_rolling.sql
@@ -1,13 +1,18 @@
-CREATE VIEW Regional_Price_Trends AS
+CREATE VIEW Regional_Price_Trends_Optimized AS
+
 SELECT
-    Region,
-    Transaction_Date,
-    Sold_Price,
-	
-    AVG(Sold_Price) OVER (
-        PARTITION BY Region
-        ORDER BY Transaction_Date
-        ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+    t1.Region,
+    t1.Transaction_Date,
+    t1.Daily_Avg_Price,
+
+    (
+        SELECT AVG(t2.Daily_Avg_Price)
+        FROM Daily_Regional_Summary t2
+        WHERE t2.Region = t1.Region
+        AND julianday(t2.Transaction_Date)
+            BETWEEN julianday(t1.Transaction_Date) - 29
+            AND julianday(t1.Transaction_Date)
     ) AS Rolling_30Day_Avg
-	
-FROM Housing_Transactions_Master;
+
+FROM Daily_Regional_Summary t1;
+
